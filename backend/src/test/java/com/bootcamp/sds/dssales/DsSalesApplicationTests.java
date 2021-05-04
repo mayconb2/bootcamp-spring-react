41d2135b61103d6a398ba14d10c373308aca0fdd
Maycon de Souza <mayconb2@gmail.com>
2021-05-03 21:20:36 -0300

    Project Created

---
+++ backend/src/test/java/com/bootcamp/sds/dssales/DsSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.sds.dssales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
