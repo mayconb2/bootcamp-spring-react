1baf14c463322a6c6081c183463743b4fc525997
Maycon de Souza <mayconb2@gmail.com>
2021-05-07 20:58:55 -0300

    Home, navigation

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div>
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1 className="display-4">DS-Sales</h1>
+                        <p className="lead">See all perspectives from the sales of your company</p>
+                        <hr />
+                        <p>This application was developed with React and retrieve data from a backend using Java and Spring.</p>
+                        <Link className="btn btn-primary btn-large  " to="dashboard">
+                            Access Dashboard
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     )
 }
 
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/maycon-de-souza-485114164/" target="_blank" rel="noreferrer">Maycon de Souza</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">Web app developed by <a href="https://www.linkedin.com/in/maycon-de-souza-485114164/" target="_blank" rel="noreferrer">Maycon de Souza</a></p>
+                <p className="text-light"><small><strong>Week Spring React</strong><br />
+                Event promoted by escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
